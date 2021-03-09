8dd0ed48364482f2440e4efdd3b3a5313cad2071
mckechnie534 <79905229+mckechnie534@users.noreply.github.com>
2021-03-09 11:11:38 +0000

    added new project and test script to my unity project

---
+++ New Unity Project/Assets/scripts/TestScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+    //This variable is editable in Unity    
+    public string startMessage;
+    public string updateMessage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogError(startMessage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogWarning(updateMessage);
+    }
+}
